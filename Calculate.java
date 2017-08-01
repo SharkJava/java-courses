b6e9878b3514344a34fcd803832b960d02ee4098
SharkJava <77_igor_77@mail.ru>
2017-08-01 03:02:06 +0300

    Calculate #2

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,13 @@ public class Calculate {
 
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		System.out.println("Result: " + first / second);
+
+		long a = Long.valueOf(arg[2]);
+		long b = Long.valueOf(arg[3]);
+		System.out.println("Result 2: " +  Math.pow(a, b));
 
 	}
 
